165d02a196d70b42be9b94915b71d4cddd1bd60e
Edward Stell <edward.j.stell@gmail.com>
2021-12-05 22:01:38 +0000

    comment distribution operations

--- distribution.go
+++ distribution.go
@@ -1,11 +1,15 @@
 package card
 
+// Distribution operator should return false when the desired deal distribution
+// is met.
 type Distribution func(i int, count int) bool
 
+// Unequal distribution; deal all cards.
 var Unequal = Distribution(func(i, count int) bool {
 	return true
 })
 
+// Equal distribution; stop dealing when largest count multiple is reached.
 var Equal = Distribution(func(i, count int) bool {
 	return (len(Cards) - i) > (len(Cards) % count)
 })
