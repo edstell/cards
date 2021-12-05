b325e0fd3b6d1c4f66dd5575a91cf571ba381152
Edward Stell <edward.j.stell@gmail.com>
2021-12-05 20:59:58 +0000

    exhaustive unit testing of provided sorting methods

--- deck_test.go
+++ deck_test.go
@@ -75,6 +75,28 @@ func TestDeck_Sort(t *testing.T) {
 				CA, DA, HA, SA,
 			},
 		},
+		{
+			description: "assert deck is correctly sorted in descending suit order",
+			deck:        Cards,
+			less:        SuitDesc,
+			expectedResult: Deck{
+				SA, S2, S3, S4, S5, S6, S7, S8, S9, ST, SJ, SQ, SK,
+				HA, H2, H3, H4, H5, H6, H7, H8, H9, HT, HJ, HQ, HK,
+				DA, D2, D3, D4, D5, D6, D7, D8, D9, DT, DJ, DQ, DK,
+				CA, C2, C3, C4, C5, C6, C7, C8, C9, CT, CJ, CQ, CK,
+			},
+		},
+		{
+			description: "assert deck is correctly sorted in ascending suit order",
+			deck: Deck{
+				SA, S2, S3, S4, S5, S6, S7, S8, S9, ST, SJ, SQ, SK,
+				HA, H2, H3, H4, H5, H6, H7, H8, H9, HT, HJ, HQ, HK,
+				DA, D2, D3, D4, D5, D6, D7, D8, D9, DT, DJ, DQ, DK,
+				CA, C2, C3, C4, C5, C6, C7, C8, C9, CT, CJ, CQ, CK,
+			},
+			less:           SuitAsc,
+			expectedResult: Cards,
+		},
 	}
 	for _, tc := range tcs {
 		t.Run(tc.description, func(t *testing.T) {
